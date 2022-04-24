b769ad3b5a37507f104d5a0885a147d3ebd9d96c
KonkenBonken <konrad05pettersson@gmail.com>
2022-04-24 23:59:09 +0200

    Fixed the dataset

--- dataset-parser.js
+++ dataset-parser.js
@@ -1,15 +1,21 @@
 const fs = require('fs').promises;
+const csv = require('csvtojson')
 
 const digit = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-_";
 const toB64 = x => x.toString(2).split(/(?=(?:.{6})+(?!.))/g).map(v => digit[parseInt(v, 2)]).join("");
 
-fs.readFile('dataset.raw.csv').then(async dataset => {
-	dataset = dataset.toString();
 
-	dataset = dataset.split('\n').slice(1, -1).map(line => {
-		[n, emoji, unicode, name, , , , , , , , , , , , img] = line.split(',')
+// fs.readFile('dataset.raw.csv').then(async dataset => {
+// 	dataset = dataset.toString();
+//
+// 	dataset = dataset.split('\n').slice(1, -1).map(line => {
+// 		[n, emoji, unicode, name, , , , , , , , , , , , img] = line.split(',')
+csv().fromFile('dataset.raw.csv').then(async dataset => {
+	dataset = dataset.map(line => {
 
-		img = img.replace(/=*"$/, '')
+		let { emoji, unicode, name, JoyPixels: img } = line
+
+		img = img.substr(56).replace(/=*"$/, '')
 
 		unicode = unicode.split(' ').map(x =>
 			toB64(parseInt(x.substr(2), 16))
@@ -22,7 +28,7 @@ fs.readFile('dataset.raw.csv').then(async dataset => {
 		return [unicode, name, img].join(',');
 		// unicode -> num -> b64
 		// name -> % = " face " -> + = " with "
-		// img -> b64 image -> add "data:image/png;base64,"
+		// img -> b64 image -> add "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEgAAABICA"
 	}).join(';')
 	fs.writeFile('dataset.csv', dataset)
 });
