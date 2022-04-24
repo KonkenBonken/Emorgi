cf5df3d72e7fa514c47f913e1818ebed578b9b35
KonkenBonken <konrad05pettersson@gmail.com>
2022-04-24 23:44:17 +0200

    Removed unused code

--- dataset-parser.js
+++ dataset-parser.js
@@ -6,12 +6,6 @@ const toB64 = x => x.toString(2).split(/(?=(?:.{6})+(?!.))/g).map(v => digit[par
 fs.readFile('dataset.raw.csv').then(async dataset => {
 	dataset = dataset.toString();
 
-	let face = 0,
-		wit = 0,
-		per = 0,
-		plus = 0,
-		facewith = 0;
-
 	dataset = dataset.split('\n').slice(1, -1).map(line => {
 		[n, emoji, unicode, name, , , , , , , , , , , , img] = line.split(',')
 
@@ -20,12 +14,6 @@ fs.readFile('dataset.raw.csv').then(async dataset => {
 		unicode = unicode.split(' ').map(x =>
 			toB64(parseInt(x.substr(2), 16))
 		).join(' ');
-
-		if (name.includes('face')) face++;
-		if (name.includes('with')) wit++;
-		if (name.includes('face with')) facewith++;
-		if (name.includes('%')) per++;
-		if (name.includes('+')) plus++;
 
 		name = name
 			.replace(/\s*face\s*/g, '%')
