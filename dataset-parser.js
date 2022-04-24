72085b15523ab57b7a0364a00503fa6d93f89bbf
KonkenBonken <konrad05pettersson@gmail.com>
2022-04-24 17:55:20 +0200

    Compress name

--- dataset-parser.js
+++ dataset-parser.js
@@ -5,14 +5,33 @@ const toB64 = x => x.toString(2).split(/(?=(?:.{6})+(?!.))/g).map(v => digit[par
 
 fs.readFile('dataset.raw.csv').then(async dataset => {
 	dataset = dataset.toString();
+
+	let face = 0,
+		wit = 0,
+		per = 0,
+		plus = 0,
+		facewith = 0;
+
 	dataset = dataset.split('\n').slice(1, -1).map(line => {
 		[n, emoji, unicode, name, , , , , , , , , , , , img] = line.split(',')
 
 		unicode = unicode.split(' ').map(x =>
 			toB64(parseInt(x.substr(2), 16))
 		).join(' ');
+
+		if (name.includes('face')) face++;
+		if (name.includes('with')) wit++;
+		if (name.includes('face with')) facewith++;
+		if (name.includes('%')) per++;
+		if (name.includes('+')) plus++;
+
+		name = name
+			.replace(/\s*face\s*/g, '%')
+			.replace(/\s*with\s*/g, '+');
+
 		return [unicode, name, img].join(',');
 		// unicode -> num -> b64
+		// name -> % = " face " -> + = " with "
 	}).join(';')
 	fs.writeFile('dataset.csv', dataset)
 });
